c1a4ecb2f5578a7bb1a97c9bf067d3024f9122c5
Elviex123 <125544065+Elviexe@users.noreply.github.com>
2024-11-29 01:28:36 +0800

    Update 10642.cpp

--- 一星題/_34/10642.cpp
+++ 一星題/_34/10642.cpp
@@ -1,22 +1,30 @@
 // https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=1583
 
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-  int num, x1, y1, x2, y2; // 變數 num 總共會有幾項數據，變數 x1 y1 是 source circle 座標，變數 x2 y2 是 destination circle 座標
+int main() {
+    int num, x1, y1, x2, y2; 
+    //num=>測試資料數量 (x1, y1)和(x2, y2)分別為起點與終點的座標
 
-  while (cin >> num)
-    for (int i = 1; i <= num; ++i)
-    {
-      cin >> y1 >> x1 >> y2 >> x2; // 輸入 x1, y1, x2, y2
+    while (cin >> num) { // 讀取測試資料的數量
+        for (int i = 1; i <= num; ++i) 
+        { // 處理每組測試資料
+            cin >> y1 >> x1 >> y2 >> x2; 
+            // 讀取座標 (注意：題目要求是 y 在前，x 在後)
 
-      long long n = (x1 + y1) * (x1 + y1 + 1LL) / 2LL + y1; // 照著公式做，其中 1LL 和 2LL 是 long long的 1 和 2，得出 n
-      long long m = (x2 + y2) * (x2 + y2 + 1LL) / 2LL + y2; // 照著公式做，其中 1LL 和 2LL 是 long long的 1 和 2，得出 m
+            // 計算起點與終點的序列位置
+            long long n = (x1 + y1) * (x1 + y1 + 1LL) / 2LL + y1; 
+            // 起點 P(x1, y1)
+   
 
-      cout << "Case " << i << ": " << (m - n) << endl; // m - n  就是每個 Case 的解答
+            long long m = (x2 + y2) * (x2 + y2 + 1LL) / 2LL + y2; 
+            // 終點 P(x2, y2)
+
+            
+            cout << "Case " << i << ": " << (m - n) << endl;
+            // 輸出結果，計算步數差距
+        }
     }
-  return 0;
+    return 0;
 }
